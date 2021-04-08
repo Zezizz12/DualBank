e20ab9cdc8b4a9bef152a93ca090fbbf7335bc48
José Vitor <jvitor.on2@gmail.com>
2021-04-08 12:24:27 -0300

    terceiro commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const cors= require ("cors");
+
+app.use (cors());
 
 ReactDOM.render(
   <React.StrictMode>
